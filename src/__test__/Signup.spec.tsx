228a60b74d64d9b67a24f930bbb8ec296561ebd2
JongMany <blackberry1114@naver.com>
2024-12-05 08:10:49 +0900

    회원가입 성공 케이스에 대한 테스트

--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -8,8 +8,7 @@ const queryClient = new QueryClient({
   defaultOptions: {},
 });
 describe("회원가입 테스트", () => {
-  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러 메시지가 표시된다.", async () => {
-    // given - 회원가입 페이지가 그려짐
+  beforeEach(() => {
     const routes = [
       {
         path: "/signup",
@@ -25,6 +24,11 @@ describe("회원가입 테스트", () => {
         <RouterProvider router={router} />
       </QueryClientProvider>
     );
+  });
+
+  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러 메시지가 표시된다.", async () => {
+    // given - 회원가입 페이지가 그려짐
+    // beforeEach에서 구현
 
     // when - 비밀번호와 비밀번호 확인 값이 일치하지 않는다.
     const passwordInput = screen.getByLabelText("비밀번호");
@@ -38,5 +42,28 @@ describe("회원가입 테스트", () => {
     // then - 에러 메시지가 표시된다.
     const errorMessage = await screen.findByTestId("error-message");
     expect(errorMessage).toBeInTheDocument();
+  });
+
+  test("이메일을 입력하고, 비밀번호와 비밀번호 확인값이 일치하면 회원가입 버튼이 활성화된다.", () => {
+    //Given - 회원가입 페이지가 그려짐
+    // beforeEach에서 구현
+    // When - 이메일 입력, 비밀번호, 비밀번호 확인 일치
+    const signupButton = screen.getByRole("button", { name: /회원가입/i });
+    expect(signupButton).toBeDisabled();
+
+    const emailInput = screen.getByLabelText("이메일");
+    const passwordInput = screen.getByLabelText("비밀번호");
+    const confirmPasswordInput = screen.getByLabelText("비밀번호 확인");
+
+    fireEvent.change(emailInput, {
+      target: { value: "blackberry1114@naver.com" },
+    });
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.change(confirmPasswordInput, {
+      target: { value: "password" },
+    });
+
+    // Then - 회원가입 버튼 활성화
+    expect(signupButton).toBeEnabled();
   });
 });
