853cbada8bc2da7a4906f027b00f10daae426f70
JongMany <blackberry1114@naver.com>
2024-12-07 17:30:18 +0900

    배달 버튼 클릭 시 페이지 이동 테스트

---
+++ cypress/e2e/order/order.cy.ts
@@ -0,0 +1,11 @@
+describe("", () => {
+  it("", () => {
+    cy.visit("/");
+
+    cy.get("[data-cy=deliveryBtn]").should("be.visible").as("deliveryBtn");
+    cy.get("[data-cy=pickupBtn]").should("be.visible").as("pickupBtn");
+
+    cy.get("@deliveryBtn").click();
+    cy.url().should("include", "/food-type");
+  });
+});
